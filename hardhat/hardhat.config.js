db29021a6e9303df0e14e420d1ce8bb8f462f59a
Githaiga22 <allangithaiga5@gmail.com>
2025-07-23 23:06:24 +0300

    add modified configuration with CELO alfajores

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    celo: {
+      url: process.env.CELO_ALFAJORES_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      celo: process.env.CELOSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "celo",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io",
+        },
+      },
+    ],
+  },
 };
